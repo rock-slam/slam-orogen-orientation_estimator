8ede9d8060e707632a1bf6ae6141e94fc75ccaac
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2012-11-03 16:07:05 +0100

    [IKFEstimator]: improved the run ruby script

--- scripts/run_ikf_stim300.rb
+++ scripts/run_ikf_stim300.rb
@@ -14,6 +14,8 @@ end
 #Initializes the CORBA communication layer
 Orocos.initialize
 
+proprio = {:proprio => true}
+
 Orocos.run('ikf_orientation_estimator_test') do 
   
     # log all the output ports
@@ -23,12 +25,19 @@ Orocos.run('ikf_orientation_estimator_test') do
     # get the invidual tasks
     ikf_attitude_task = TaskContext.get 'ikf_orientation_estimator'
     Orocos.conf.apply(ikf_attitude_task, ['stim300'], :override => true	)
-    stim300 = TaskContext.get 'stim300'
-
-         
-    #Mapping the inputs ports in the ikf orientation task
-    stim300.calibrated_sensors.connect_to( ikf_attitude_task.imu_samples, :type => :buffer, :size => 10 )
     
+    if proprio[:proprio]
+	proprio = TaskContext.get 'propriocessing'
+
+	#Mapping the inputs ports in the ikf orientation task
+	proprio.calibrated_sensors_out.connect_to( ikf_attitude_task.imu_samples, :type => :buffer, :size => 10 )
+    else
+	stim300 = TaskContext.get 'stim300'
+	
+	#Mapping the inputs ports in the ikf orientation task
+	stim300.calibrated_sensors.connect_to( ikf_attitude_task.imu_samples, :type => :buffer, :size => 10 )
+    end
+
     ikf_attitude_task.configure
     ikf_attitude_task.start
 
