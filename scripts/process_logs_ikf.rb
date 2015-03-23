1d92d8da3ef143fcdb6fba059200a4c3eb72b303
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-03-24 00:09:50 +0100

    new ruby script and config file

---
+++ scripts/process_logs_ikf.rb
@@ -0,0 +1,42 @@
+#! /usr/bin/env ruby
+
+require 'orocos'
+require 'orocos/log'
+require 'vizkit'
+
+include Orocos
+
+if ARGV.size < 1 then
+    puts "usage: process_logs_ikf.rb <data_log_directory>"
+    exit
+end
+
+#Initializes the CORBA communication layer
+Orocos.initialize
+
+
+Orocos::Process.run 'orientation_estimator::IKF' => 'ikf_orientation_estimator' do
+
+    Orocos.conf.load_dir('./config')
+    ikf_attitude_task = TaskContext.get 'ikf_orientation_estimator'
+    Orocos.conf.apply(ikf_attitude_task, ['default', 'Bremen', 'stim300_10g'], :override => true)
+
+    # connect the tasks to the logs
+    log_replay = Orocos::Log::Replay.open( ARGV[0] )
+
+    #Mapping the inputs ports in the orientation task
+    log_replay.imu_stim300.calibrated_sensors.connect_to( ikf_attitude_task.imu_samples, :type => :buffer, :size => 10 )
+
+    # log all the output ports
+    Orocos.log_all_ports
+
+    ikf_attitude_task.configure
+    ikf_attitude_task.start
+
+    # open the log replay widget
+    control = Vizkit.control log_replay
+    control.speed = 1 #4
+
+    Vizkit.exec
+
+end
