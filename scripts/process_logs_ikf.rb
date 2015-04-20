c947e50aa4f9db8322e1a8a36a1811f177494243
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-04-20 18:23:27 +0200

    ruby test file loading different configurations

--- scripts/process_logs_ikf.rb
+++ scripts/process_logs_ikf.rb
@@ -19,13 +19,16 @@ Orocos::Process.run 'orientation_estimator::IKF' => 'ikf_orientation_estimator' 
 
     Orocos.conf.load_dir('./config')
     ikf_attitude_task = TaskContext.get 'ikf_orientation_estimator'
-    Orocos.conf.apply(ikf_attitude_task, ['default', 'Bremen', 'stim300_10g'], :override => true)
+   # Orocos.conf.apply(ikf_attitude_task, ['default', 'Bremen', 'stim300_10g'], :override => true)
+   # Orocos.conf.apply(ikf_attitude_task, ['default', 'DLR-Oberpfaffenhofen', 'stim300_5g'], :override => true)
+    Orocos.conf.apply(ikf_attitude_task, ['default', 'DLR-Oberpfaffenhofen', 'virtual_imu'], :override => true)
 
     # connect the tasks to the logs
     log_replay = Orocos::Log::Replay.open( ARGV[0] )
 
     #Mapping the inputs ports in the orientation task
-    log_replay.imu_stim300.calibrated_sensors.connect_to( ikf_attitude_task.imu_samples, :type => :buffer, :size => 10 )
+    #log_replay.imu_stim300.calibrated_sensors.connect_to(ikf_attitude_task.imu_samples, :type => :buffer, :size => 10)
+    log_replay.imu_stim300.inertial_sensors_out.connect_to(ikf_attitude_task.imu_samples, :type => :buffer, :size => 10)
 
     # log all the output ports
     Orocos.log_all_ports
