d6ca1fcc20fabfcee5d5aa21accc25da4ce479d2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-06-21 21:55:38 -0300

    remove usage of deprecated (now removed) headers

--- orientationTypes.hpp
+++ orientationTypes.hpp
@@ -8,8 +8,8 @@
  * which case you do not need this file
  */
 
-#include <base/time.h>
-#include <base/eigen.h>
+#include <base/Time.hpp>
+#include <base/Eigen.hpp>
 
 namespace orientation_estimator
 {
